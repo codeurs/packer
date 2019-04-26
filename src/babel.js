9b548df4ebad11d67064d95ff613c58534a340a7
Ben Merckx <ben@codeurs.be>
2019-04-26 12:37:15 +0200

    Try transform runtime again

--- src/babel.js
+++ src/babel.js
@@ -1,26 +1,28 @@
 module.exports = {
-  cacheDirectory: true,
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        modules: false,
-        loose: true,
-        targets: {
-          browsers: ['last 2 versions', 'ie >= 9', 'safari >= 7']
-        }
-      }
-    ]
-  ],
-  plugins: [
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-syntax-jsx',
-    '@babel/plugin-transform-proto-to-assign',
-    ['@babel/plugin-proposal-decorators', {legacy: true}],
-    ['@babel/plugin-proposal-class-properties', {loose: true}],
-    '@babel/plugin-proposal-object-rest-spread',
-    ['@babel/plugin-transform-react-jsx', {pragma: 'm'}],
-    ['@babel/plugin-transform-destructuring', {useBuiltIns: true}]
-    //['@babel/plugin-transform-runtime', {corejs: 2}]
-  ]
+	cacheDirectory: true,
+	presets: [
+		[
+			'@babel/preset-env',
+			{
+				useBuiltIns: 'usage',
+				corejs: 3,
+				modules: false,
+				loose: true,
+				targets: {
+					browsers: ['last 2 versions', 'ie 11', 'safari >= 10']
+				}
+			}
+		]
+	],
+	plugins: [
+		'@babel/plugin-syntax-dynamic-import',
+		'@babel/plugin-syntax-jsx',
+		'@babel/plugin-transform-proto-to-assign',
+		['@babel/plugin-proposal-decorators', {legacy: true}],
+		['@babel/plugin-proposal-class-properties', {loose: true}],
+		'@babel/plugin-proposal-object-rest-spread',
+		['@babel/plugin-transform-react-jsx', {pragma: 'm'}],
+		['@babel/plugin-transform-destructuring', {useBuiltIns: true}],
+		['@babel/plugin-transform-runtime', {corejs: 3}]
+	]
 }
